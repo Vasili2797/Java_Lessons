d7d64455abc13e332bb9ae8f503768dbb5a34c8a
Vasili2797 <nashvilliv1@gmail.com>
2021-12-03 17:57:09 -0500

    lesson14_abstraction lesson working files

---
+++ src/com/Personal_Java_Lessons/lesson14_abstraction/Tesla.java
@@ -0,0 +1,8 @@
+package com.Personal_Java_Lessons.lesson14_abstraction;
+
+public class Tesla extends Car{
+    @Override
+    public void foo() {
+        System.out.println("Tesla foo function");
+    }
+}
