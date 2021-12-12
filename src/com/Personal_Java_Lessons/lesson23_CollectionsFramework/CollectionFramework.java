d1b443e96e9c8b02c11274ebc2a30d48c0d76f63
Vasili2797 <nashvilliv1@gmail.com>
2021-12-12 14:53:29 -0500

    lesson 23

---
+++ src/com/Personal_Java_Lessons/lesson23_CollectionsFramework/CollectionFramework.java
@@ -0,0 +1,11 @@
+package com.Personal_Java_Lessons.lesson23_CollectionsFramework;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class CollectionFramework {
+
+    //Collection;
+    //Map;
+
+}
