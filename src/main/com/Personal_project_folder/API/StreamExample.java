4a8f01151263e00c8b158368b32eb9f9f95dd866
Vasili2797 <nashvilliv1@gmail.com>
2022-05-13 13:28:52 -0400

    Fixed a few insignificant bugs in the code

--- src/main/com/Personal_project_folder/API/StreamExample.java
+++ src/main/com/Personal_project_folder/API/StreamExample.java
@@ -1,6 +1,7 @@
 package main.com.Personal_project_folder.API;
 
 import java.util.Comparator;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamExample {
@@ -8,7 +9,8 @@ public class StreamExample {
         //count(), min(), and max() methods
 
         Stream<Integer> stream = Stream.of(1,2,3,4,5,6,7,8,9,10);
-        System.out.println(stream.count());
+        System.out.println(stream.collect(Collectors.toList()));
+        System.out.println("The number of elements in the Stream are: "+Stream.of(1,2,3,4,5,6,7,8,9,10).count());
 
         Integer min = Stream.of(1,2,3,4,5,6,7,8,9,10).min(Comparator.comparing(Integer::valueOf)).get();
         System.out.println("Minimum value in the List: "+min);
