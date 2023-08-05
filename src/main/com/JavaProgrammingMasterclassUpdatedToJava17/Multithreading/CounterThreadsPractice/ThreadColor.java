52d26bf9e5928a973b2d4e8287a3e60183c7744b
Vasili2797 <nashvilliv1@gmail.com>
2023-08-05 13:03:42 -0400

    multithreading lessons

---
+++ src/main/com/JavaProgrammingMasterclassUpdatedToJava17/Multithreading/CounterThreadsPractice/ThreadColor.java
@@ -0,0 +1,11 @@
+package main.com.JavaProgrammingMasterclassUpdatedToJava17.Multithreading.CounterThreadsPractice;
+
+public class ThreadColor {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK= "\u001B[30m";
+    public static final String ANSI_RED="\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_BLUE= "\u001B[34m";
+    public static final String ANSI_PURPLE="\u001B[35m";
+    public static final String ANSI_CYAN = "\u100B[36m";
+}
---
+++ src/main/com/JavaProgrammingMasterclassUpdatedToJava17/Multithreading/ProducerConsumerMultiThreadingPractice/Main.java
@@ -0,0 +1,7 @@
+package main.com.JavaProgrammingMasterclassUpdatedToJava17.Multithreading.ProducerConsumerMultiThreadingPractice;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
